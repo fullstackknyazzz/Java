2be82fa58fa773948dfb05918f5783db0a9d7cff
vladimir360 <vladimir360@mail.ru>
2023-06-25 21:46:52 +0300

    add one task

---
+++ 1/twoProgram.java
@@ -0,0 +1,18 @@
+public class twoProgram {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1001; i++) {
+            // поставили флаг
+            boolean isPrime = true;  
+            for (int j = 2; j <= Math.floor(Math.sqrt(i)); j++){
+                //Если j - делитель, флаг = false
+                if ((i % j) == 0) { 
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.print(i + " "); //если делителей нет, флаг остался True, число на печать
+            }
+        }
+    }
+}
